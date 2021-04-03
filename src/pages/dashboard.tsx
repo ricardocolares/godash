b4089a98c520840dc6f274c80cbef585e6a24597
Ricardo Colares <ricardoccolares@gmai.com>
2021-04-03 18:09:21 -0300

    sidebar component created

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,15 @@
+import { Flex } from "@chakra-ui/layout";
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 }
